034b4a247b26fe6f29d68f22599c5372e75bc3da
Arthur Lee <arthur.lee945@gmail.com>
2024-05-21 22:16:19 -0500

    updating base struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     println!("Intro");
     intro();
@@ -5,6 +7,16 @@ fn main() {
     variables();
     println!("data types");
     data_types();
+    println!("console input");
+    console_input();
+    println!("arithmatic and casting");
+    arithmatic_casting();
+    println!("conditional");
+    conditional();
+    println!("functions");
+    function_info();
+    println!("memory management");
+    memory_mgmt();
 }
 
 fn intro() {
@@ -54,3 +66,59 @@ fn data_types() {
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
     println!("{}", arr[3]);
 }
+
+fn console_input() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line");
+    println!("{}", input);
+}
+
+fn arithmatic_casting() {
+    let x: u8 = 18;
+    let y: i8 = 19;
+    let z = x + (y as u8);
+
+    println!("{}", z);
+    //casting to f32
+    let _casted = 3.7_f32;
+    let _casted = 3.7f32;
+    let _casted = 3.7 as f32;
+    //str conv
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("expected to readline");
+    let int_input: i64 = input.trim().parse().unwrap();
+    println!("{}", int_input + 5);
+}
+
+fn conditional() {
+    let _cond = 2 < 9; // 2 < 3.9 is invalid need types to match
+    let _compound = !(2 < 9 && true || false);
+
+    let food = "kbbq";
+    if food == "kbbq" {
+        println!("nice");
+    } else if food == "sushi" {
+        println!("also good choice");
+    } else {
+        println!("no food for you");
+    }
+}
+
+fn function_info() {
+    fn add_numbers(x: i32, y: i32) -> i32 {
+        // x + y without ; will return as well
+        return x + y;
+    }
+    println!("{}", add_numbers(19, 3));
+    let number = {
+        let x = 34;
+        x + 1
+    };
+    println!("{}", number);
+}
+
+fn memory_mgmt() {}
