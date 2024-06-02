f2531d8b5d877af79502e39f49bbb5b04b3254ac
Arthur Lee <arthur.lee945@gmail.com>
2024-06-02 13:36:29 -0500

    adding common type functions

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ intro_proj:
 	cargo run -p intro
 guess:
 	cargo run -p guess_this
+common: 
+	cargo run -p common_concept
---
+++ common_concept/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    returning_loop();
+    println!(
+        "This is 84 deg fahrenheight to celsius: {}",
+        fahrenheith_to_celsius(84)
+    );
+    println!("This is 88th fibonacci number: {}", fibonacci(88));
+}
+
+fn returning_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 2;
+        if counter > 12 {
+            break counter * 8;
+        }
+    };
+    println!("This is return value of loop: {}", result);
+}
+
+fn fahrenheith_to_celsius(deg: i32) -> i32 {
+    (deg - 32) * 5 / 9
+}
+
+fn fibonacci(n: usize) -> usize {
+    let mut cache = vec![0, 1];
+    if n <= 1 {
+        return n;
+    }
+    for i in 2..=n {
+        cache.push(cache[i - 1] + cache[i - 2]);
+    }
+    cache[n]
+}
