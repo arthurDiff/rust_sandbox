3296ef1beac31232a2a99cc8677218f59ffbbf12
Arthur Lee <arthur.lee945@gmail.com>
2024-06-04 22:20:11 -0500

    starting on module proj

---
+++ module_sys/src/lib.rs
@@ -0,0 +1,10 @@
+mod front_of_house {
+    #[allow(dead_code)]
+    mod hosting {
+        fn add_to_waitlist() {}
+    }
+    #[allow(dead_code)]
+    mod serving {
+        fn take_order() {}
+    }
+}
