a3a95583316c9813d62ee50d0b9e242f36c616b4
Arthur Lee <arthur.lee945@gmail.com>
2024-07-20 23:01:06 -0500

    block new impl

--- blockchain_p/src/block.rs
+++ blockchain_p/src/block.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use crate::transaction::Transaction;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -7,4 +9,19 @@ pub struct Block {
     pub transaction: Transaction,
     pub proof: u32,
     pub previous_hash: String,
+}
+
+impl Block {
+    pub fn new(index: u32, tx: Transaction, proof: u32, prev_hash: String) -> Self {
+        Self {
+            index,
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            transaction: tx,
+            proof,
+            previous_hash: prev_hash,
+        }
+    }
 }
