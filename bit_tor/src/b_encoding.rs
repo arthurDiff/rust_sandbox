50b996b6c5724f6fbf3e2b766e69541e2a3231bb
arthurDiff <arthur.lee945@gmail.com>
2025-04-30 21:50:38 -0500

    add be decoder

--- bit_tor/src/b_encoding.rs
+++ bit_tor/src/b_encoding.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, error::Error, iter::Peekable, slice::Iter};
+use std::{error::Error, iter::Peekable, slice::Iter};
+
+use indexmap::IndexMap;
 
 const DICT_START: u8 = b'd';
 const DICT_END: u8 = b'e';
@@ -37,8 +39,8 @@ pub enum Value {
     Bool(bool),
     Number(i128),
     String(String),
-    Array(Vec<Value>),
-    Object(HashMap<String, Value>),
+    List(Vec<Value>),
+    Object(IndexMap<String, Value>),
 }
 
 pub struct BEncoding;
@@ -62,21 +64,67 @@ impl BEncoding {
             return Ok(Value::Null);
         };
 
-        // rewrite this
         Ok(match **next_b {
+            DICT_START => Value::Object(Self::decode_dict(b_iter)?),
+            LIST_START => Value::List(Self::decode_list(b_iter)?),
+            NUM_START => Value::Number(Self::decode_number(b_iter)?),
             _ => Value::String(Self::decode_byte_arr(b_iter)?),
         })
     }
 
+    fn decode_dict(b_iter: &mut Peekable<Iter<u8>>) -> Result<IndexMap<String, Value>> {
+        // move over DICT_START
+        _ = b_iter.next();
+
+        let mut idx_map = IndexMap::new();
+        while let Some(b) = b_iter.peek() {
+            if **b == DICT_END {
+                // move over DICT_END
+                _ = b_iter.next();
+                break;
+            }
+
+            idx_map.insert(Self::decode_byte_arr(b_iter)?, Self::decode_next(b_iter)?);
+        }
+
+        Ok(idx_map)
+    }
+
+    fn decode_list(b_iter: &mut Peekable<Iter<u8>>) -> Result<Vec<Value>> {
+        // move over LIST_START
+        _ = b_iter.next();
+
+        let mut list = vec![];
+        while let Some(b) = b_iter.peek() {
+            if **b == LIST_END {
+                // move over LIST_END
+                _ = b_iter.next();
+                break;
+            }
+
+            list.push(Self::decode_next(b_iter)?);
+        }
+
+        Ok(list)
+    }
+
     // number decoder
     fn decode_number(b_iter: &mut Peekable<Iter<u8>>) -> Result<i128> {
-        Ok(String::from_utf8_lossy(
+        // move over NUM_START
+        _ = b_iter.next();
+
+        let n = String::from_utf8_lossy(
             &b_iter
                 .take_while(|v| **v != NUM_END)
                 .cloned()
                 .collect::<Vec<u8>>(),
         )
-        .parse::<i128>()?)
+        .parse::<i128>()?;
+
+        // move over NUM_END
+        _ = b_iter.next();
+
+        Ok(n)
     }
 
     // string decoder
